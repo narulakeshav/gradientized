7ed2ec4d771f0d53c79751ea2acce297f62413c4
Keshav Narula <NarulaKeshav13@gmail.com>
2016-05-26 17:11:40 -0700

    Clean up code and add comments

--- public/js/gradient.js
+++ public/js/gradient.js
@@ -1,42 +1,51 @@
 $(document).ready(function() {
-    // Initializing WOW JS and Tooltip
-    new WOW().init();
+
+    // INITIALIZES CLIPBOARD ON HTML ELEMENTS
     new Clipboard('.btn-clipboard');
     new Clipboard('#first');
     new Clipboard('#second');
     
-    // Getting HTML Elements by ID
+    // GETS HTML VALUES FROM IDs
     var d = document;
-    var bigDifference = false;
     var code = '-webkit-linear-gradient(#6D78E0, #A486E0);';
     var next = d.getElementById('next');
     var gradientDiv = d.getElementById('gradient');
+
+    // CREATS FIRSTCOLOR AND SECONDCOLOR VARIABLES
     var firstColor;
     var secondColor;
 
-    // Messsage Box
+    // MESSAGE BOX DOM
     var message = d.getElementById("box");
-    var messageType = d.getElementById("message-type");
     var mainMessage = d.getElementById("main-message");
     var messageParent = d.getElementById("message-parent");
 
     // SHOWS DIFFERENT GRADIENT ON DIV/BUTTON CLICK
     next.onclick = function() {
+        // GENERATES RANDOM HEXCODE AND ASSIGNS CODE
         firstColor = generateHex();
         secondColor = generateHex();
+
+        // GENERATES BACKGROUND CSS CODE
         code = '-webkit-linear-gradient(left, #' + firstColor + ', #' + secondColor + ')';
-        $('.gradient').css('background', code);
-        $('#first').css('background', '#' + firstColor);
-        $('#second').css('background', '#' + secondColor);
-        $('#first').html('#' + firstColor);
-        $('#second').html('#' + secondColor);
+        
+        // CHANGES BACKROUND GRADIENT AND BUTTON COLORS
+        changeStyle('.gradient', 'background', code);
+        changeStyle('#first', 'background', ('#' + firstColor));
+        changeStyle('#second', 'background', ('#' + secondColor));
+        changeStyle('.btn-clipboard', 'background', code);
+        changeStyle('.comment', 'color', ('#' + secondColor));
+
+        // SETS CLIPBOARD TEXT TO THE UPDATED COLOR
         $('#first').attr('data-clipboard-text', '#' + firstColor);
         $('#second').attr('data-clipboard-text', '#' + secondColor);
-        $('.btn-clipboard').css('background', code);
-        $('#line_1').html('background: #' + firstColor + ';');
-        $('#line_2').html('background: -webkit-linear-gradient(90deg, ' + '#' + firstColor + ', #' + secondColor + ');');
-        $('#line_3').html('background: linear-gradient(90deg, ' + '#' + firstColor + ', #' + secondColor + ');');
-        $(".comment").css('color', '#' + secondColor);
+
+        // UPDATES HTML VALUES
+        updateHTML('#first', '#' + firstColor);
+        updateHTML('#second', '#' + secondColor);
+        updateHTML('#line_1', 'background: #' + firstColor + ';');
+        updateHTML('#line_2', 'background: -webkit-linear-gradient(90deg, ' + '#' + firstColor + ', #' + secondColor + ');');
+        updateHTML('#line_3', 'background: linear-gradient(90deg, ' + '#' + firstColor + ', #' + secondColor + ');');
     };
 
     $('.btn-clipboard').click(function() {
@@ -51,6 +60,7 @@ $(document).ready(function() {
         displayMessage();
     });
 
+    /* GENERATES A RANDOM HEXCODE */
     function generateHex() {
         var arr = ['A', 'B', 'C', 'D', 'E', 'F', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
         var code = '';
@@ -61,6 +71,7 @@ $(document).ready(function() {
         return code;
     }
 
+    /* CONVERTS HEXCODE TO RGB CODE */
     function hexToRgb(hex) {
         var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
         return result ? {
@@ -70,14 +81,22 @@ $(document).ready(function() {
         } : null;
     }
 
+    /* DISPLAYS MESSAGE BOX WHEN TEXT COPIED TO CLIPBOARD */
     function displayMessage() {
         changeStyle(".message-box", "display", "block");
+
+        // HIDES THE MESSAGE-BOX IN 4 SECONDS
         setTimeout(function() {
             changeStyle(".message-box", "display", "none");
         }, 4000);
     }
 
+    /* CHANGES CSS STYLING */
     function changeStyle(element, property, value) {
         $(element).css(property, value);
+    }
+
+    function updateHTML(element, value) {
+        $(element).html(value);
     }
 });
